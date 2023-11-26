9bb77e31fe8ed82d0fe1a442f2119c0c37267519
khalil <K.aitjakane@gmail.com>
2023-11-26 18:17:18 +0000

    Implement the logic in romantoint and intotoroman functions

--- numberconversion.h
+++ numberconversion.h
@@ -3,21 +3,47 @@
 
 #include <string>
 
-/*
-  Convert a number from the Roman numeral representation to an
-  integer.
-  @param roman_numeral the Roman numeral representation of the number
-  to be converted.
-  @return integer representing the Roman numerals.
-*/
-int romantoint(std::string roman_numeral);
+int romantoint(const std::string& roman_numeral) {
+    std::map<char, int> romanValues = {
+        {'I', 1}, {'V', 5}, {'X', 10},
+        {'L', 50}, {'C', 100}, {'D', 500},
+        {'M', 1000}
+    };
 
-/*
-  Convert a number from an integer to the Roman numeral
-  representation.
-  @param number the integer to be converted to Roman numerals.
-  @return Roman numeral representation of the number.
-*/
-std::string inttoroman(int number);
+    int total = 0;
+    int prev_value = 0;
+
+    for (int i = roman_numeral.length() - 1; i >= 0; --i) {
+        int current_value = romanValues[roman_numeral[i]];
+
+        if (current_value < prev_value)
+            total -= current_value;
+        else
+            total += current_value;
+
+        prev_value = current_value;
+    }
+
+    return total;
+}
+
+
+std::string inttoroman(int number) {
+    std::vector<std::pair<int, std::string>> romanValues = {
+        {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+        {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+        {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
+    };
+
+    std::string roman = "";
+    for (const auto &pair : romanValues) {
+        while (number >= pair.first) {
+            roman += pair.second;
+            number -= pair.first;
+        }
+    }
+
+    return roman;
+}
 
 #endif
